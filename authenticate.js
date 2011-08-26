4ccd36038f530372c22a03f3427efaa0fd2b419e
Zach Hodge <qcom100@gmail.com>
2011-08-25 20:23:58 -0700

    - auth

--- authenticate.js
+++ authenticate.js
@@ -1,21 +1,20 @@
-function auth(){
-  var crypto = require('crypto');
+var crypto = require('crypto');
 
-  /* Authentication */
+/* Authentication */
 
-  // Salt Generator
-  function generateSalt(){
-    var text = "";
-    var possible= "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*"
-    for(var i = 0; i < 40; i++)
-      text += possible.charAt(Math.floor(Math.random() * possible.length));
+// Salt Generator
+function generateSalt(){
+  var text = "";
+  var possible= "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*"
+  for(var i = 0; i < 40; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
     return text;
   }
 
-  // Generate Hash
-  function hash(msg, key){
-    return crypto.createHmac('sha256', key).update(msg).digest('hex');
-  }
+// Generate Hash
+function hash(msg, key){
+  return crypto.createHmac('sha256', key).update(msg).digest('hex');
 }
+
 exports.generateSalt = generateSalt;
 exports.hash = hash;
